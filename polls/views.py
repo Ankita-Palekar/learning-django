b51594180e1ae01559c60632cc7b2abe9b022762
Ankita-Palekar <453ankitapalekar@gmail.com>
2017-09-05 16:16:44 +0530

    test

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index 1.")
 
 # Create your views here.
